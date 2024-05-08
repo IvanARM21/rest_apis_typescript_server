b637c1205913d9a181dc39a7d97d2044566d347d
IvanARM21 <ivanrms371@gmail.com>
2024-05-07 22:22:51 -0300

    First Commit

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import colors from 'colors';
+import cors, { CorsOptions } from 'cors'
+import morgan from 'morgan'
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec, { swaggerUiOptions } from './config/swagger';
+import router from './router';
+import db from './config/db';
+
+// Conectar a base de datos
+export async function connectDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        // console.log(colors.blue('Conexión Exiosta a la BD'));
+    } catch (error) {
+        console.log(error);
+        console.log( colors.red.bold('Hubo un error al conectar a la BD'));
+    }
+}
+connectDB();
+
+// Instancia de express
+const server = express();
+
+// Permitir conexiones
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        if(origin === process.env.FRONTENT_URL) {
+            callback(null, true);
+        } else {
+            callback(new Error('Error de CORS'));
+        }
+    }
+}
+server.use(cors(corsOptions));
+
+// Leer datos de formularios
+server.use(express.json());
+
+server.use(morgan('dev'));
+
+server.use('/api/products', router);
+
+server.get('/api', (req,res) => {
+    res.json({msg: 'desde api'})
+});
+
+// Docs
+server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions));
+
+export default server;
